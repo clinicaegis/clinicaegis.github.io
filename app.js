91ab316b2ebabe57bd1aa3b83b292b05cf554fa5
clinicaegis <rambertm@gmail.com>
2023-07-04 16:13:37 +0900

    app set

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+
+app.use('/css', express.static('css'));
+app.use('/script', express.static('script'));
+
 app.get('', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
